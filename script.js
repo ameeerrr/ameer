22897a386055c42040478c8e0e78f9d6a9a33d22
ameeerrr <ameerkurdy21@gmail.com>
2024-09-22 12:55:11 -0400

    update

--- script.js
+++ script.js
@@ -52,10 +52,13 @@ lngSelector.addEventListener("change",(e)=>{
 setLang(e.target.value);
 localStorage.setItem("lang",e.target.value);
 })
-document.addEventListener("DOMContentLoaded",()=> {
-    
-    setLang(localStorage.getItem("lang"));
-})
+document.addEventListener("DOMContentLoaded", () => {
+    // Check if a language is stored in localStorage, otherwise default to "ar"
+    let lang = localStorage.getItem("lang") || "ar"; // "ar" as the default language
+    setLang(lang); // Set the language on page load
+    document.querySelector("select").value = lang; // Set the default value of the selector
+});
+
 const setLang = (lang) => {
 const elements = document.querySelectorAll("[data-i18n]");
 elements.forEach((element)=> {
@@ -79,4 +82,3 @@ let lastLi = document.getElementById("last");
     logo.style.justifyContent="" ;
 }
 }
-
